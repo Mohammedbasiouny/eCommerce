32d6faea54f058d2a48fee8153dc93e746074326
Mohamed Basiouny <125449711+Mohammedbasiouny@users.noreply.github.com>
2024-06-12 09:36:07 +0300

    Update bootstrap.css and backend.css styles

--- admin/includes/templates/header.php
+++ admin/includes/templates/header.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-    <link rel="stylesheet" href="<?php echo $css;?>backend.css">
-    <link rel="stylesheet" href="<?php echo $css;?>bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo $css;?>fontawesome.min.css">
+    <link rel="stylesheet" href="<?php echo $css ?>bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php echo $css ?>fontawesome.min.css" />
+    <link rel="stylesheet" href="<?php echo $css ?>jquery-ui.css" />
+    <link rel="stylesheet" href="<?php echo $css ?>jquery.selectBoxIt.css" />
+    <link rel="stylesheet" href="<?php echo $css ?>backend.css" />
 </head>
 
 <body>
