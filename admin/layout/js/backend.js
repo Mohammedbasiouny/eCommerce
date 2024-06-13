b7261c13cbfa903119d4ef557cd6400b7376527c
Mohamed Basiouny <125449711+Mohammedbasiouny@users.noreply.github.com>
2024-06-13 12:49:11 +0300

    Update dashboard and member pages with improved performance and functionality 60!!

--- admin/layout/js/backend.js
+++ admin/layout/js/backend.js
@@ -1,115 +1,91 @@
 $(function () {
+	"use strict";
 
-	'use strict';
+	// Dashboard
 
-	// Dashboard 
+	$(".toggle-info").click(function () {
+		$(this)
+			.toggleClass("selected")
+			.parent()
+			.next(".panel-body")
+			.fadeToggle(100);
 
-	$('.toggle-info').click(function () {
-
-		$(this).toggleClass('selected').parent().next('.panel-body').fadeToggle(100);
-
-		if ($(this).hasClass('selected')) {
-
+		if ($(this).hasClass("selected")) {
 			$(this).html('<i class="fa fa-minus fa-lg"></i>');
-
 		} else {
-
 			$(this).html('<i class="fa fa-plus fa-lg"></i>');
-
 		}
-
 	});
 
 	// Trigger The Selectboxit
 
 	$("select").selectBoxIt({
-
-		autoWidth: false
-
+		autoWidth: false,
 	});
 
 	// Hide Placeholder On Form Focus
 
-	$('[placeholder]').focus(function () {
+	$("[placeholder]")
+		.focus(function () {
+			$(this).attr("data-text", $(this).attr("placeholder"));
 
-		$(this).attr('data-text', $(this).attr('placeholder'));
-
-		$(this).attr('placeholder', '');
-
-	}).blur(function () {
-
-		$(this).attr('placeholder', $(this).attr('data-text'));
-
-	});
+			$(this).attr("placeholder", "");
+		})
+		.blur(function () {
+			$(this).attr("placeholder", $(this).attr("data-text"));
+		});
 
 	// Add Asterisk On Required Field
 
-	$('input').each(function () {
-
-		if ($(this).attr('required') === 'required') {
-
+	$("input").each(function () {
+		if ($(this).attr("required") === "required") {
 			$(this).after('<span class="asterisk">*</span>');
-
 		}
-
 	});
 
 	// Convert Password Field To Text Field On Hover
 
-	var passField = $('.password');
+	var passField = $(".password");
 
-	$('.show-pass').hover(function () {
-
-		passField.attr('type', 'text');
-
-	}, function () {
-
-		passField.attr('type', 'password');
-
-	});
+	$(".show-pass").hover(
+		function () {
+			passField.attr("type", "text");
+		},
+		function () {
+			passField.attr("type", "password");
+		}
+	);
 
 	// Confirmation Message On Button
 
-	$('.confirm').click(function () {
-
-		return confirm('Are You Sure?');
-
+	$(".confirm").click(function () {
+		return confirm("Are You Sure?");
 	});
 
 	// Category View Option
 
-	$('.cat h3').click(function () {
-
-		$(this).next('.full-view').fadeToggle(200);
-
+	$(".cat h3").click(function () {
+		$(this).next(".full-view").fadeToggle(200);
 	});
 
-	$('.option span').click(function () {
+	$(".option span").click(function () {
+		$(this).addClass("active").siblings("span").removeClass("active");
 
-		$(this).addClass('active').siblings('span').removeClass('active');
-
-		if ($(this).data('view') === 'full') {
-
-			$('.cat .full-view').fadeIn(200);
-
+		if ($(this).data("view") === "full") {
+			$(".cat .full-view").fadeIn(200);
 		} else {
-
-			$('.cat .full-view').fadeOut(200);
-
+			$(".cat .full-view").fadeOut(200);
 		}
-
 	});
 
 	// Show Delete Button On Child Cats
 
-	$('.child-link').hover(function () {
-
-		$(this).find('.show-delete').fadeIn(400);
-
-	}, function () {
-
-		$(this).find('.show-delete').fadeOut(400);
-
-	});
-
+	$(".child-link").hover(
+		function () {
+			$(this).find(".show-delete").fadeIn(400);
+		},
+		function () {
+			$(this).find(".show-delete").fadeOut(400);
+		}
+	);
 });
